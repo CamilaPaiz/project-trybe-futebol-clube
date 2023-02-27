ecc0c6201d90fd40b117821629713cab2b4ab3d2
CamilaPaiz <camilapaiz@hotmail.com>
2023-02-27 18:26:52 -0300

    feat: desenvolvidas migration e model para tabela times

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING(255),
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
